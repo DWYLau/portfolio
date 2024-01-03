77aa489b027d7b2698827db42ffee73f7d16703a
Derek <dereklau15@gmail.com>
2024-01-03 22:28:30 +0900

    Update skills, revamp project section, change color palette

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-let menuIcon = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+let menuIcon = document.querySelector("#menu-icon")
+let navbar = document.querySelector(".navbar")
 
 menuIcon.onclick = () => {
-  menuIcon.classList.toggle("bx-x");
-  navbar.classList.toggle("active");
-};
+  menuIcon.classList.toggle("bx-x")
+  navbar.classList.toggle("active")
+}
 
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+let sections = document.querySelectorAll("section")
+let navLinks = document.querySelectorAll("header nav a")
 
 window.onscroll = () => {
-  sections.forEach((section) => {
-    let top = window.scrollY;
-    let offset = section.offsetTop - 150;
-    let height = section.offsetHeight;
-    let id = section.getAttribute("id");
+  sections.forEach(section => {
+    let top = window.scrollY
+    let offset = section.offsetTop - 150
+    let height = section.offsetHeight
+    let id = section.getAttribute("id")
 
     if (top >= offset && top < offset + height) {
-      navLinks.forEach((link) => {
-        link.classList.remove("active");
+      navLinks.forEach(link => {
+        link.classList.remove("active")
         document
           .querySelector("header nav a[href*=" + id + "]")
-          .classList.add("active");
-      });
+          .classList.add("active")
+      })
     }
-  });
+  })
 
-  let header = document.querySelector("header");
+  let header = document.querySelector("header")
 
-  header.classList.toggle("sticky", window.scrollY > 100);
+  header.classList.toggle("sticky", window.scrollY > 100)
 
-  menuIcon.classList.remove("bx-x");
-  navbar.classList.remove("active");
-};
+  menuIcon.classList.remove("bx-x")
+  navbar.classList.remove("active")
+}
 
-ScrollReveal({ distance: "80px", duration: 2000, delay: 200 });
+ScrollReveal({ distance: "80px", duration: 2000, delay: 200 })
 
-ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" })
 ScrollReveal().reveal(
-  ".home-img, .services-container, .portfolio-box, .contact form",
+  ".home-img, .services-container, .portfolio-container, .portfolio-container-alt, .contact form",
   { origin: "bottom" }
-);
-ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+)
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" })
 ScrollReveal().reveal(".home-content p, .home-content ul, .about-content", {
   origin: "right",
-});
+})
 
 const typed = new Typed(".multiple-text", {
   strings: ["Frontend Developer", "Designer", "Programmer"],
@@ -52,4 +52,4 @@ const typed = new Typed(".multiple-text", {
   backSpeed: 100,
   backDelay: 1000,
   loop: true,
-});
+})
